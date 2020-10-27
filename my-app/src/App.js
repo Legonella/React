45dc4a885eda89a70db13462ddd26182938be7ef
Legonella <dastbravis@gmail.com>
2020-10-27 15:55:44 +0100

    Distributed components to files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,15 @@
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
 
 
 const App = () => {
   return (
 		<div>
-			<div><Header /></div>
-			<div className = "App">
-				<ul>
-					<li>css</li>
-					<li>html</li>
-					<li>js</li>
-					<li>react</li>
-				</ul>
-			</div>
+			<Header />
+			<Technologies />
 		</div>
   );
-}
-
-const Header  = () => {
-	return (<div>
-	<a href = "#s">Hello</a>
-	<a href = "#s">News</a>
-	<a href  = "#s">Const</a>
-	</div>
-	);
 }
 
 export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+	return(
+		<div>
+			<a href = '#s'>Home</a>
+			<a href = '#s'>Contact</a>
+			<a href = '#s'>About me</a>
+		</div>
+	);
+}
+
+export default Header;

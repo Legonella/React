8ba9eb42669e97dfaf970a08a9bca01c25b09eb5
Legonella <dastbravis@gmail.com>
2020-10-29 11:00:57 +0100

    Breakdown into components

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+	return(
+		<header className = 'header'>
+		<img alt = ""  src = 'https://cdn.vox-cdn.com/thumbor/p01ezbiuDHgRFQ-htBCd7QxaYxo=/0x105:2012x1237/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg'/>
+	</header>
+	)
+}
+
+export default Header;
---
+++ social-network/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+	return(
+		<nav className = "nav">
+		<div><a href="#s" className = "link">Profile</a></div>
+		<div><a href="#s" className = "link">Messages</a></div>
+		<div><a href="#s" className = "link">News</a></div>
+		<div><a href="#s" className = "link">Music</a></div>
+		<div><a href="#s" className = "link">Settings</a></div>
+		</nav>
+	)
+}
+
+export default Nav;
